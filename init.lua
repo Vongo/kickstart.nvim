0f54aaa3c176ce14bc56b1948768cfe2bb1d1387
Adrian <adelmarre@vongo.eu>
2024-09-11 15:08:12 +0200

    [Update][Plugin-Flash] Added Flash + config, removed ufo.

--- init.lua
+++ init.lua
@@ -876,13 +876,20 @@ require('lazy').setup({
     end,
   },
   {
-    'kevinhwang91/nvim-ufo',
-    dependencies = { -- Used for async loading of plugins
-      'kevinhwang91/promise-async', -- Used for async loading of plugins
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {
+      jump = { auto_jump = true },
     },
-    config = function()
-      require 'kickstart.plugins.nvim-ufo'
-    end,
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
