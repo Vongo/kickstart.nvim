4748cb8cbfb75044245a7511a6810c28562fc3ac
Adrian <adelmarre@vongo.eu>
2024-09-13 18:45:59 +0200

    [Update][Plugin] Added LSP-signature.

--- init.lua
+++ init.lua
@@ -902,6 +902,14 @@ require('lazy').setup({
   {
     'lukas-reineke/lsp-format.nvim',
   },
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
