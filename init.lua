886d883d5926584289890d6ff8bcd90d68b25f3c
Adrian <adrian.delmarre@gmail.com>
2025-06-08 16:33:33 +0200

    [Update][Config-Plugins] Added avante.

--- init.lua
+++ init.lua
@@ -1424,7 +1424,100 @@ require('lazy').setup({
       }
     end,
   },
-
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
+    keys = {
+      {
+        '<leader>a+',
+        function()
+          local tree_ext = require 'avante.extensions.nvim_tree'
+          tree_ext.add_file()
+        end,
+        desc = 'Select file in NvimTree',
+        ft = 'NvimTree',
+      },
+      {
+        '<leader>a-',
+        function()
+          local tree_ext = require 'avante.extensions.nvim_tree'
+          tree_ext.remove_file()
+        end,
+        desc = 'Deselect file in NvimTree',
+        ft = 'NvimTree',
+      },
+    },
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = 'openai',
+      providers = {
+        openai = {
+          endpoint = 'https://api.openai.com/v1',
+          model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+          extra_request_body = {
+            timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+            temperature = 0.75,
+            max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+          },
+        },
+        claude = {
+          endpoint = 'https://api.anthropic.com',
+          model = 'claude-sonnet-4-20250514',
+          timeout = 30000, -- Timeout in milliseconds
+          -- disable_tools = true, -- disable tools!
+          extra_request_body = {
+            temperature = 0,
+            max_tokens = 4096,
+          },
+        },
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'ibhagwan/fzf-lua', -- for file_selector provider fzf
+      'stevearc/dressing.nvim', -- for input provider dressing
+      'folke/snacks.nvim', -- for input provider snacks
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
   {
     'frankroeder/parrot.nvim',
     dependencies = { 'ibhagwan/fzf-lua', 'nvim-lua/plenary.nvim' },
@@ -1621,8 +1714,8 @@ vim.cmd.colorscheme 'solarized-osaka'
 vim.api.nvim_set_keymap('n', '<c-p>', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<c-p>', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<c-k>', '<cmd>LspStop<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<c-k>', '<cmd>LspStop<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<c-k>', '<cmd>LspStop r-language-server<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<c-k>', '<cmd>LspStop r-language-server<cr>', { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<C-f>', '<cmd>lua require("telescope.builtin").live_grep()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<C-f>', '<cmd>lua require("telescope.builtin").live_grep()<CR>', { noremap = true, silent = true })
